e1d245b3fd74162502ace613087dbd32c7527a68
fortz007 <fortuneojih@yahoo.com>
2023-05-03 11:37:58 +0100

    created ResponseManager util

---
+++ src/main/java/com/fortuneprogramming/activitytrackerapplication/utils/ResponseManager.java
@@ -0,0 +1,16 @@
+package com.fortuneprogramming.activitytrackerapplication.utils;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class ResponseManager<T> {
+    public ApiResponse<T> success(T data){
+        return new ApiResponse<T>((T) "Request Successful",true, data);
+    }
+
+    public ApiResponse<T> error(T errorMessage){
+        return new ApiResponse<T>(errorMessage, false, null);
+    }
+}
